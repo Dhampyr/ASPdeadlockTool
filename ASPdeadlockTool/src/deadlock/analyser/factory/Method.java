63fab1851105ce06f2db4ae91f3f171ba10f8ea7
Dhampyr <vincenzo.mastandrea.1986@gmail.com>
2015-05-20 10:42:45 +0200

    20/05/2015

---
+++ ASPdeadlockTool/src/deadlock/analyser/factory/Method.java
@@ -0,0 +1,24 @@
+package deadlock.analyser.factory;
+
+import java.util.LinkedList;
+
+import com.gzoumix.semisolver.term.Term;
+import com.gzoumix.semisolver.term.TermVariable;
+import com.gzoumix.semisolver.term.Variable;
+
+public class Method extends GenericStructuredTerm {
+
+public static String name = "Method";
+public String methodName = "";
+	
+	public Method(String methodName) {
+		super(name, new LinkedList<Term>());
+		this.methodName = methodName;
+	}
+	
+	public String toString(){
+		String kind = ".";
+		return kind;
+	}
+	
+}
